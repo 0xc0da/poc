a2a020242c1109e035a5ef400b9eb6fab339eb35
0xc0da <daniel@codalabs.net>
2013-09-15 11:29:13 -0400

    Comments added. IP is now a command line argument.

--- dummy-http-server/httpserver-0.3.py
+++ dummy-http-server/httpserver-0.3.py
@@ -1,3 +1,21 @@
+#!-*- coding: utf8 -*-
+#
+# PoC - Dummy HTTP Server
+# Armazena o valor do campo User-Agent de qualquer request, e
+# retorna 200 OK.
+#
+# Uso: python http-server-0.3.py <IP> <Port> <Logfile>
+# Ex.: python http-server-0.3.py 127.0.0.1 8080 ua.txt
+#
+# Versao: 0.3 - 15/Set/2013
+#
+# Daniel Marques - daniel /arroba\ codalabs /ponto\ net
+# http://codalabs.net
+#
+# Changelog
+# v0.3~> Versão inicial.
+
+
 from BaseHTTPServer import HTTPServer,BaseHTTPRequestHandler
 import urlparse
 import sys
@@ -5,8 +23,8 @@ import sys
 class RequestHandler(BaseHTTPRequestHandler):
 
     def generic_handling(self):
-        with open(sys.argv[2],'a') as logfile:
-            line = self.headers["user-agent"]# + "\n"
+        with open(sys.argv[3],'a') as logfile:
+            line = self.headers["user-agent"]
             logfile.write(line + '\n')
         self.send_response(200)
 	for name,value in self.headers.items():
@@ -28,11 +46,12 @@ class RequestHandler(BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print "Use: %s <Port> <Logfile>" % (sys.argv[0])
+        print "Use: %s <IP> <Port> <Logfile>" % (sys.argv[0])
         sys.exit(1)
 
-    port = int(sys.argv[1])
+    host = sys.arv[1]
+    port = int(sys.argv[2])
 
-    server = HTTPServer(('localhost', port), RequestHandler)    
+    server = HTTPServer((host, port), RequestHandler)    
     print 'Server started, use <Ctrl-C> to stop'
     server.serve_forever()    
